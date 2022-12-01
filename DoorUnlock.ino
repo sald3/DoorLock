b7f6cca95b18d313bf0d739664311241003d9f89
sald3 <91228996+sald3@users.noreply.github.com>
2022-12-01 17:26:01 -0500

    Add files via upload

--- DoorUnlock.ino
+++ DoorUnlock.ino
@@ -3,13 +3,17 @@ Servo myServo;                              // create a servo object called mySe
 const int TrigPin = 2;
 const int EchoPin = 3;
 const int Light = 7;
+const int Buttons = 8;
 float cm;
+bool onOff = false;
+
 void setup()
 {
   myServo.attach(8);                          // attaches the servo on pin 8 to the myServo object
   pinMode(TrigPin, OUTPUT);
   pinMode(EchoPin, INPUT);
   pinMode(Data,OUTPUT);
+  pinMode(Buttons,INPUT);
   Serial.begin(9600);
 }
 void loop()
@@ -25,13 +29,23 @@ void loop()
   if(cm<15){ //if you are puting the sensor behind the door use ">" sysmbole insted of ">" p.s. make sure tat the door nob is less than 15 cm away
       digitalWrite(Light,HIGH);
       myServo.write(50);
-      delay(200);
+      delay(2000);
+      onOff = true;
     }
-    else
-    {
+
+   if(digitalRead(Buttons) == HIGH){
+    digitalWrite(Light,HIGH);
+      myServo.write(50);
+      delay(2000);
+      onOff = true;
+   }
+
+    if(onOff == true){
       digitalWrite(Light,LOW);
       myServo.write(25);
-      delay(200);  
+      delay(200); 
+      onOff = false; 
     }
-  delay(200);
+   
+  delay(20);
 }
